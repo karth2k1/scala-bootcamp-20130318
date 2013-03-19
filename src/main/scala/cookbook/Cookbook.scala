cee0ae648c87d292fcd97e9c1a0bbd3c1a3ed72a
Dan Rosen <dr@mergeconflict.com>
2013-03-19 14:34:46 -0700

    unapply!

--- src/main/scala/cookbook/Cookbook.scala
+++ src/main/scala/cookbook/Cookbook.scala
@@ -5,6 +5,9 @@ class Recipe(val ingredients: List[String], val directions: List[String])
 object Recipe {
   def apply(ingredients: List[String], directions: List[String]) =
     new Recipe(ingredients, directions)
+  
+  def unapply(recipe: Recipe): Option[(List[String], List[String])] =
+    Some((recipe.ingredients, recipe.directions))
 }
 
 class Cookbook(val recipes: Map[String, Recipe])
@@ -14,4 +17,7 @@ object Cookbook {
     new Cookbook(recipes)
   def empty: Cookbook =
     new Cookbook(Map.empty)
+
+  def unapply(cookbook: Cookbook): Option[Map[String, Recipe]] =
+    Some(cookbook.recipes)
 }
