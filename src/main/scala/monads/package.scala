4a2121c0d3aaa0675d1a0a6d3647a9e563e52666
Dan Rosen <dr@mergeconflict.com>
2013-03-21 10:59:39 -0700

    option monad implementation

--- src/main/scala/monads/package.scala
+++ src/main/scala/monads/package.scala
@@ -7,14 +7,14 @@ package object monads {
   }
 
   case class Some[+A](get: A) extends Option[A] {
-    def map[B](fn: A => B): Option[B] = ???
-    def flatMap[B](fn: A => Option[B]): Option[B] = ???
+    def map[B](fn: A => B): Option[B] = Some(fn(get))
+    def flatMap[B](fn: A => Option[B]): Option[B] = fn(get)
   }
 
   case object None extends Option[Nothing] {
     def get: Nothing = throw new NoSuchElementException
-    def map[B](fn: Nothing => B): Option[B] = ???
-    def flatMap[B](fn: Nothing => Option[B]): Option[B] = ???
+    def map[B](fn: Nothing => B): Option[B] = None
+    def flatMap[B](fn: Nothing => Option[B]): Option[B] = None
   }
 
 }
