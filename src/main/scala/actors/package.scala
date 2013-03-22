ef26baa2dbbb4235dd5ed9db9cad0c7c7e15e7f0
Dan Rosen <dr@mergeconflict.com>
2013-03-22 12:22:19 -0700

    logger outline

--- src/main/scala/actors/package.scala
+++ src/main/scala/actors/package.scala
@@ -1,5 +1,35 @@
+import util.Random
+
+import java.util.concurrent._
+import collection.mutable
+
 package object actors {
 
-  def fib(n: Int): BigInt = ???
+  def fib(n: Int): BigInt = n match {
+    case 0 | 1 => 1
+    case _ => fib(n - 1) + fib(n - 2)
+  }
+
+  def randomFib: BigInt = fib(Random nextInt 35)
+
+  class Logger extends Runnable {
+    private[this] val messages = mutable.Queue.empty[String]
+
+    def log(message: String): Unit = ???
+    def run(): Unit = ???
+  }
+
+  def main(args: Array[String]): Unit = {
+    val pool = Executors.newFixedThreadPool(10)
+
+    for (_ <- 1 to 20) {
+      val task = new Runnable {
+        def run(): Unit = println(randomFib)
+      }
+      pool execute task
+    }
+
+    pool.shutdown()
+  }
 
 }
